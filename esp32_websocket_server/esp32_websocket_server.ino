675d20ca34073bf7217c1bf4680d89167292803d
aleweb04 <aleweber2004@gmail.com>
2025-10-16 15:18:42 +0200

    websocket on esp32 working

--- esp32_websocket_server/esp32_websocket_server.ino
+++ esp32_websocket_server/esp32_websocket_server.ino
@@ -3,30 +3,29 @@
 #include <ESPAsyncWebServer.h>
 #include "website.cpp"
 
-const char* ssid = "Linkem_FCD881_EXT";
-const char* password = "d29vust7";
+const char *ssid = "ALEWEB";
+const char *password = "password";
 
-bool ledState = 0;
-const int ledPin = 23;
+const int motPin = 23;
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 AsyncWebSocket ws("/ws");
 
-void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {   // called on websocket's incoming message
-  AwsFrameInfo *info = (AwsFrameInfo*)arg;
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {  // called on websocket's incoming message
+  AwsFrameInfo *info = (AwsFrameInfo *)arg;
   if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
     data[len] = 0;
-    Serial.println((char*)data);  // print data recived from websocket
-    if (strcmp((char*)data, "toggle") == 0) {
-      ledState = !ledState;
-      ws.textAll(String(ledState));   // send a broadcast message
-    }
+
+    /* CODICE ESEGUITO QUANDO RICEVO UN MESSAGGIO DAL WS */
+    String payload = String((char *)data);
+    Serial.println(payload);  // print data recived from websocket
+
+    // ws.textAll(String(ledState));  // send a broadcast message
   }
 }
 
-void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
-             void *arg, uint8_t *data, size_t len) {
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
   switch (type) {
     case WS_EVT_CONNECT:
       Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
@@ -43,16 +42,15 @@ void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType 
   }
 }
 
-String processor(const String& var) {   // replace %DATA% in html file
+String processor(const String &var) {  // replace %DATA% in html file
   Serial.println(var);
-  if (var == "STATE") {
-    if (ledState) {
-      return "ON";
-    }
-    else {
-      return "OFF";
-    }
-  }
+  // if (var == "STATE") {
+  //   if (ledState) {
+  //     return "ON";
+  //   } else {
+  //     return "OFF";
+  //   }
+  // }
   return String();
 }
 
@@ -60,8 +58,8 @@ void setup() {
   // Serial port for debugging purposes
   Serial.begin(115200);
 
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);
+  pinMode(motPin, OUTPUT);
+  digitalWrite(motPin, LOW);
 
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
@@ -76,9 +74,9 @@ void setup() {
   // initialize websocket
   ws.onEvent(onEvent);
   server.addHandler(&ws);
-  
+
   // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send_P(200, "text/html", index_html, processor);
   });
 
@@ -87,6 +85,6 @@ void setup() {
 }
 
 void loop() {
-  ws.cleanupClients();      // delete disconnected clients 
-  digitalWrite(ledPin, ledState);
+  ws.cleanupClients();  // delete disconnected clients
+  
 }
