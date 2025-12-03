706f2213527804fa3e50d61999bd4d6020b22eb5
aleweb04 <aleweber2004@gmail.com>
2025-12-03 15:05:22 +0100

    mex nd webstate

---
+++ websocket py test/Untitled-1.js
@@ -0,0 +1,27 @@
+import WebSocket from "ws";
+
+const ip = "127.0.0.1";
+const port = 81;
+
+const ws = new WebSocket(`ws://${ip}:${port}`);
+
+ws.on("open", () => {
+    console.log("Connesso al server.");
+
+    const msg = "ciao server!";
+    console.log("Invio:", msg);
+    ws.send(msg);
+});
+
+ws.on("message", (data) => {
+    console.log("Risposta:", data.toString());
+});
+
+// Rimane aperto senza chiudere
+ws.on("close", () => {
+    console.log("Connessione chiusa dal server.");
+});
+
+ws.on("error", (err) => {
+    console.error("Errore:", err);
+});
